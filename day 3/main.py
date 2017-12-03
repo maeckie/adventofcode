8425b13115ea752bd48224d4b0520abc2af35196
marcusdahlstein <marcus.dahlstein@leogears.com>
2017-12-03 19:13:15 +0100

    2nd part. Code keeps getting worse and worse

--- day 3/main.py
+++ day 3/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import math
+import numpy as np
 
 #37  36  35  34  33  32  31
 #38  17  16  15  14  13  30
@@ -10,75 +11,88 @@ import math
 #43  44  45  46  47  48  49
 
 puzzle_input=289326
-#puzzle_input=12
-
-lower_right_corner = 1
-square_size = 1
-laps = 1
-
-while lower_right_corner < puzzle_input:
-    square_size = square_size + 2
-    lower_right_corner = square_size * square_size
-    print lower_right_corner
-    laps=laps+1
-
-print "------"
-
-#print lower_right_corner
-#print square_size
-#print puzzle_input
-#distance from center to edge of spiral
-print "SQ Size: %d" % (square_size)
-print "Laps: %d" % laps
-to_edge = laps-1
-for i in range(0,4):
-
-    lower_right_corner = lower_right_corner - (square_size-1)
-    print "lower_right: %d" % lower_right_corner
-    to_mid_on_edge = 0
-
-    if lower_right_corner < puzzle_input:
-        print "in the loop: %d" % i
-        print i
-        print puzzle_input
-        print lower_right_corner
-        mid_on_edge = math.ceil(float(square_size)/float(2))
-        pos_on_edge = puzzle_input - lower_right_corner + 1
-
-        if pos_on_edge > mid_on_edge:
-            to_mid_on_edge = pos_on_edge - mid_on_edge
-        else:
-            to_mid_on_edge = mid_on_edge - pos_on_edge
-
-        print "mid_on_edge: %d" % mid_on_edge
-        print "to_mid_on_edge: %d" % (to_mid_on_edge)
-        print "to_edge: %d" % (to_edge)
-        print "Manhattan: %d" % (to_mid_on_edge + to_edge)
-        break
-
-# for i in range(1,5):
-#     lower_right_corner = lower_right_corner - square_size-1
-#     if lower_right_corner < puzzle_input:
-#         edge_pos = puzzle_input - lower_right_corner
-#         print "edge_pos_1: %d" % (edge_pos)
-#         mid_pos = (square_size-1)/2+1
-#         if edge_pos > mid_pos:
-#             distance_to_mid = edge_pos - mid_pos
-#         else:
-#             distance_to_mid = mid_pos - edge_pos
-#
-#         print "to_edge: %d" % (to_edge)
-#         print "distance_to_mid: %d" % (distance_to_mid)
-#         print "edge_pos: %d" % (edge_pos)
-#         print "mid_pos: %d" % (mid_pos)
-#         manhattan_distance = to_edge + distance_to_mid
-#
-# print manhattan_distance
-
-#find where we are on the edge
-#while lower_right_corner > puzzle_input:
 
 
+def part1():
+    lower_right_corner = 1
+    square_size = 1
+    laps = 1
 
-#manhattan_distance = to_edge + edge_distance_from_mid
-#print manhattan_distance
+    while lower_right_corner < puzzle_input:
+        square_size = square_size + 2
+        lower_right_corner = square_size * square_size
+        laps=laps+1
+
+    to_edge = laps-1
+    for i in range(0,4):
+
+        lower_right_corner = lower_right_corner - (square_size-1)
+        to_mid_on_edge = 0
+
+        if lower_right_corner < puzzle_input:
+            mid_on_edge = math.ceil(float(square_size)/float(2))
+            pos_on_edge = puzzle_input - lower_right_corner + 1
+
+            if pos_on_edge > mid_on_edge:
+                to_mid_on_edge = pos_on_edge - mid_on_edge
+            else:
+                to_mid_on_edge = mid_on_edge - pos_on_edge
+
+            print "Manhattan: %d" % (to_mid_on_edge + to_edge)
+            break
+
+
+def getSumNeighbours(matrix, x, y):
+    nSum = 0
+    nSum = nSum + matrix[x-1][y-1]
+    nSum = nSum + matrix[x][y-1]
+    nSum = nSum + matrix[x+1][y-1]
+    nSum = nSum + matrix[x-1][y]
+    nSum = nSum + matrix[x+1][y]
+    nSum = nSum + matrix[x-1][y+1]
+    nSum = nSum + matrix[x][y+1]
+    nSum = nSum + matrix[x+1][y+1]
+
+    return nSum
+
+def part2():
+    matrix = np.zeros((100,100), dtype=np.int)
+
+    x = 50
+    y = 50
+    matrix[x][y] = 1
+    d = 1
+    steps = 1
+    it = 0
+    exit = 0
+    while 1:
+        for t in range(0,2):
+            for i in range(0,steps):
+
+                direction = [[1,0], [0,1], [-1,0], [0,-1]]
+                x = x + direction[d][0]
+                y = y + direction[d][1]
+
+
+                matrix[x][y] = getSumNeighbours(matrix, x, y)
+                if matrix[x][y] > puzzle_input:
+                    print matrix[x][y]
+
+                    exit = 1
+                    break
+
+
+            if exit == 1:
+                break
+            d = d + 1
+            if d > 3:
+                d = 0
+
+
+        steps = steps + 1
+        if exit == 1:
+            break
+
+
+#part1()
+part2()
