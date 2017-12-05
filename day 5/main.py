7f5a732f60f4320e0032454f5ae1b315b4a3cd81
marcusdahlstein <marcus.dahlstein@leogears.com>
2017-12-05 08:23:49 +0100

    parts 1 and 2

---
+++ day 5/main.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+with open('/Users/marcus/Documents/advent/adventofcode/day 5/input.txt', 'r') as myfile:
+    puzzle_input=myfile.readlines()
+
+#puzzle_input = [0,3,0,1,-3]
+puzzle_input = [x.strip() for x in puzzle_input]
+puzzle_input = map(int, puzzle_input)
+
+def part1():
+
+    i = 0
+    steps = 0
+    while 1:
+        if i > len(puzzle_input)-1:
+            print "Out of the maze in %d steps" % steps
+            break
+        current = puzzle_input[i]
+        puzzle_input[i] += 1
+        i += current
+        steps += 1
+
+def part2():
+    i = 0
+    steps = 0
+    while 1:
+        if i > len(puzzle_input)-1:
+            print "Out of the maze in %d steps" % steps
+            print puzzle_input
+            break
+        current = puzzle_input[i]
+        if puzzle_input[i] >= 3:
+            puzzle_input[i] -= 1
+        else:
+            puzzle_input[i] += 1
+        i += current
+        steps += 1
+
+#part1()
+part2()
