535508cbfe59d77c0c6b8b03e88c5b6923949766
marcusdahlstein <marcus.dahlstein@leogears.com>
2017-12-17 22:19:19 +0100

    Finally got the mod calculations right.... Makes everything much faster

--- day 17/main.py
+++ day 17/main.py
@@ -15,6 +15,16 @@ def part1():
     print circular_buffer[pos+1]
 
 
+def part1_better():
+    circular_buffer = [0]
+    pos = 0
+    for i in range(1,2018):
+        pos = (367 + pos) % i
+        pos += 1
+        circular_buffer.insert(pos, i)
+    print circular_buffer[pos+1]
+
+
 def part2():
     circular_buffer = deque([0])
     for i in range(1,50000000):
@@ -22,5 +32,21 @@ def part2():
         circular_buffer.append(i)
     print circular_buffer[list(circular_buffer).index(0)+1]
 
+def part2_better():
+    circular_buffer = [0]
+    pos = 0
+    keep = 0
+    for i in range(1,50000000):
+        pos = (367 + pos) % i
+        pos += 1
+        if pos == 1:
+            keep = i
+        #circular_buffer.insert(pos, i)
+
+    print keep
+    #25674054
+
 part1()
+part1_better()
 part2()
+part2_better()
